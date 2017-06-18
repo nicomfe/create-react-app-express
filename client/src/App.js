a0c624301b07eeffc9010e97be79f6d893cc1bfb
Nicolas Fetter <nicofetter@gmail.com>
2017-06-18 17:24:47 +1200

    Adding client

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { users: [] }
+  }
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    // Get the passwords and store them in state
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    const { users } = this.state
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-intro">
+          {users.map(user => <div>{user.id}</div>)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
