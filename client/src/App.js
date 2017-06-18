9aaf33f438df10c5db7a07435c1ae15596caee19
Nicolas Fetter <nicofetter@gmail.com>
2017-06-19 08:20:20 +1200

    adding twitter auth

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React2</h2>
         </div>
         <div className="App-intro">
           <a href="/api/auth/twitter/login"> Twitter Login</a>
